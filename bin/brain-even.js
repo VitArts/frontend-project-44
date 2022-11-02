128847d7172cbd61bbb6387fa606b339019157f8
VitArts <vitarts@ya.ru>
2022-11-02 14:33:35 +0300

    Этап 5

---
+++ bin/brain-even.js
@@ -0,0 +1,9 @@
+import {answer, brainEven} from '../src/even.js' 
+import welcome from '../src/cli.js'
+
+console.log('Welcome to the Brain Games!')
+console.log('Hello' + welcome() + '!')
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+answer
+brainEven()
---
+++ src/even.js
@@ -0,0 +1,41 @@
+import readlineSync from 'readline-sync' 
+
+const answer = () => {
+	const even = readlineSync.question('Your answer: ')
+	return even
+}
+
+// Основная функция
+const brainEven = () => {
+	let result = 0
+
+	for (let i = 1; i <= 3; i++) {
+		let number = Math.floor(Math.random() * 100)
+
+		console.log('Question: '+ number)
+
+		let even = answer()
+
+		if (number % 2 === 0 && even === 'yes') {
+			console.log('Correct!')
+			result += 1
+		} else if (number % 2 !== 0 && even === 'no') {
+			console.log('Correct!')
+			result += 1
+		} else if (even !== 'no' || even !== 'yes') {
+			console.log('Error!')
+			break;
+		} else {
+			console.log('Error!')
+			break;
+		}
+	}
+	
+	if (result === 3) {
+		console.log('Congratulations')
+	}
+
+}
+
+
+export {answer, brainEven}
